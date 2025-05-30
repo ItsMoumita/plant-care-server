3f3dbc060b48ff9c1dc970d276dce127466777ae
Fatema-Tuj-Zahura Moumita <fatihamoumita98@gmail.com>
2025-05-31 02:46:43 +0600

    added update functionalities by patch

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-const {ObjectId} = require('mongodb');
+const { ObjectId } = require('mongodb');
 
 
 
@@ -25,94 +25,94 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-  
+
     await client.connect();
-const plantCollection = client.db("plantCare").collection("plants");
-app.patch("/plants/:id", async (req, res) => {
-  const {id} = req.params;
-  const updatedPlant = req.body;
-  try{
-    const result = await plantCollection.updateOne(
-      { _id: new ObjectId(id) },
-      { $set: updatedPlant }
-    );
-    res.send(result);
-  }
-  catch (error) {
-    console.error("Error updating plant:", error);
-    return res.status(500).send({ error: "Failed to update plant" });
-  }
-})
-app.get("/myplants", async (req, res) => {
+    const plantCollection = client.db("plantCare").collection("plants");
+    app.patch("/plants/:id", async (req, res) => {
+      const { id } = req.params;
+      const updatedPlant = req.body;
+      try {
+        const result = await plantCollection.updateOne(
+          { _id: new ObjectId(id) },
+          { $set: updatedPlant }
+        );
+        res.send(result);
+      }
+      catch (error) {
+        console.error("Error updating plant:", error);
+        return res.status(500).send({ error: "Failed to update plant" });
+      }
+    })
+    app.get("/myplants", async (req, res) => {
 
-    const email = req.query.email;
-   
-    if(!email) {
-      return res.status(400).send({ error: "Email query parameter is required" });
+      const email = req.query.email;
+
+      if (!email) {
+        return res.status(400).send({ error: "Email query parameter is required" });
+      }
+      const result = await plantCollection.find({ email }).toArray();
+      res.send(result);
+    });
+
+    app.delete("/plants/:id", async (req, res) => {
+      const id = req.params.id;
+
+      try {
+        const result = await plantCollection.deleteOne({ _id: new ObjectId(id) });
+        res.send(result);
+      }
+      catch (error) {
+        console.error("Error deleting plant:", error);
+        return res.status(500).send({ error: "Failed to delete plant" });
+      }
+    })
+
+    app.get("/plants", async (req, res) => {
+      const result = await plantCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/plants/newplants", async (req, res) => {
+
+      const result = await plantCollection.find().sort({ nextWateringDate: 1 }).limit(6).toArray();
+      res.send(result);
+    });
+
+
+    function isValidObjectId(id) {
+      return ObjectId.isValid(id);
     }
-    const result = await plantCollection.find({ email}).toArray();
-    res.send(result);
-});
 
-app.delete("/plants/:id", async (req, res) => {
-  const id = req.params.id;
-  
-  try{
-    const result = await plantCollection.deleteOne({ _id: new ObjectId(id) });
-     res.send(result);
-  }
-  catch (error) {
-    console.error("Error deleting plant:", error);
-    return res.status(500).send({ error: "Failed to delete plant" });
-  }
-})
+    app.get("/plants/:id", async (req, res) => {
+      const id = req.params.id;
 
-app.get("/plants", async (req, res) => {
-   const result = await plantCollection.find().toArray();
-    res.send(result);
-});
-app.get("/plants/newplants" , async (req, res) => {
+      if (!isValidObjectId(id)) {
+        return res.status(400).send({ error: "Invalid plant ID" });
+      }
 
-  const result  = await plantCollection.find().sort({ nextWateringDate: 1 }).limit(6).toArray();
-  res.send(result);
-});
-
-
-function isValidObjectId(id) {
-  return ObjectId.isValid(id);
-}
-
-app.get("/plants/:id", async (req, res) => {
-  const id = req.params.id;
-
-  if (!isValidObjectId(id)) {
-    return res.status(400).send({ error: "Invalid plant ID" });
-  }
-
-  try {
-    const query = { _id: new ObjectId(id) };
-    const plant = await plantCollection.findOne(query);
-    if (!plant) {
-      return res.status(404).send({ error: "Plant not found" });
-    }
-    res.send(plant);
-  } catch (error) {
-    res.status(500).send({ error: "Failed to fetch plant" });
-  }
-});
+      try {
+        const query = { _id: new ObjectId(id) };
+        const plant = await plantCollection.findOne(query);
+        if (!plant) {
+          return res.status(404).send({ error: "Plant not found" });
+        }
+        res.send(plant);
+      } catch (error) {
+        res.status(500).send({ error: "Failed to fetch plant" });
+      }
+    });
 
 
 
 
     app.post("/plants", async (req, res) => {
       const newPlant = req.body;
-      console.log(newPlant);
-      
+     
+
       const result = await plantCollection.insertOne(newPlant);
       res.send(result);
     });
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
@@ -123,9 +123,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Hello from plant care server')
+  res.send('Hello from plant care server')
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
